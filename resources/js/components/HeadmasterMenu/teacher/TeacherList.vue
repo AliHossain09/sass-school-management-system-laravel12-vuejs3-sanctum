6e3eb9ab98db1f801c697e37f7a1f2c3af6d5f84
AloHossain09 <alihoossainn@gmail.com>
2026-01-29 11:14:38 +0600

    implement teacher dropdown menu with list, add and attendance

---
+++ resources/js/components/HeadmasterMenu/teacher/TeacherList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeadmasterLayout from '../../../layouts/HeadmasterLayout.vue'
+</script>
+
+<template>
+    <HeadmasterLayout>
+        <h1 class="text-2xl font-bold mb-6">Teacher List</h1>
+    </HeadmasterLayout> >
+</template>
+
+
+
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -6,9 +6,14 @@ import HeadmasterDashboard from '../Pages/dashboard/HeadmasterDashboard.vue';
 import TeacherDashboard from '../Pages/dashboard/TeacherDashboard.vue';
 import StudentDashboard from '../Pages/dashboard/StudentDashboard.vue';
 import ManageSchools from '../components/MasterAdminMenu/ManageSchools.vue';
+// Student Routes
 import StudentAdmission from '../components/HeadmasterMenu/student/StudentAdmission.vue';
 import StudentList from '../components/HeadmasterMenu/student/StudentList.vue';
 import StudentAttendance from '../components/HeadmasterMenu/student/StudentAttendance.vue';
+//teacher routes
+import AddTeacher from '../components/HeadmasterMenu/teacher/AddTeacher.vue';
+import TeacherList from '../components/HeadmasterMenu/teacher/TeacherList.vue';
+import TeacherAttendance from '../components/HeadmasterMenu/teacher/TeacherAttendance.vue';
 import axios from 'axios';
 
 const requireAuth = (to, from, next) => {
@@ -27,10 +32,15 @@ const routes = [
   { path: '/headmaster-dashboard', component: HeadmasterDashboard, beforeEnter: requireAuth },
   { path: '/teacher-dashboard', component: TeacherDashboard, beforeEnter: requireAuth },
   { path: '/student-dashboard', component: StudentDashboard, beforeEnter: requireAuth },
-  { path: '/manage-schools', component: ManageSchools, beforeEnter: requireAuth },,
+  { path: '/manage-schools', component: ManageSchools, beforeEnter: requireAuth },
+  // Student Routes
   { path: '/student-admission', component: StudentAdmission, beforeEnter: requireAuth },
   { path: '/students-list', component: StudentList, beforeEnter: requireAuth },
   { path: '/student-attendance', component: StudentAttendance, beforeEnter: requireAuth },
+  //teacher routes
+  { path: '/teachers-add', component: AddTeacher, beforeEnter: requireAuth },
+  { path: '/teachers-list', component: TeacherList, beforeEnter: requireAuth },
+  { path: '/teacher-attendance', component: TeacherAttendance, beforeEnter: requireAuth },
 
 ]
 
