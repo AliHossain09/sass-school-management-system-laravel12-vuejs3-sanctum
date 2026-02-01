b9c8371cb3e031ca9b85c3e1ac6082768c6bfb87
AloHossain09 <alihoossainn@gmail.com>
2026-02-01 11:44:16 +0600

    update Student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,7 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-       protected $fillable = ['student_code', 'user_id', 'school_id', 'first_name', 'last_name', 'class_id', 'section_id', 'academic_year',];
+    protected $fillable = [
+        'student_code',
+        'user_id',
+        'school_id',
+        'first_name',
+        'last_name',
+        'class_id',       // FK to school_classes
+        'section_id',     // FK to sections
+        'academic_year',
+        'father_name',
+        'father_phone',
+        'mother_name',
+        'mother_phone',
+        'local_guardian_name',
+        'local_guardian_phone',
+        'local_guardian_relationship',
+        'dob',
+        'gender',
+        'religion',
+        'nationality',
+        'email',
+        'phone',
+        'extra_curricular',
+        'description',
+        'shift',
+        'id_card_number',
+        'roll_number',
+        'board_registration_number',
+        'elective_subject_id',
+        'username',
+        'password'
+    ];
 
 
 
@@ -27,20 +58,23 @@ class Student extends Model
     // {
     //     return $this->hasMany(ParentModel::class);
     // }
-    // Section
+    
+  // Class 
+    public function class()
+    {
+        return $this->belongsTo(SchoolClass::class, 'class_id');
+    }
+
+    // Section 
     public function section()
     {
         return $this->belongsTo(Section::class, 'section_id');
     }
-    // Class
-    public function class()
+
+    // Elective subject 
+    public function electiveSubject()
     {
-        return $this->belongsTo(ClassModel::class, 'class_id');
-    }
-    // Subjects
-    public function subjects()
-    {
-        return $this->belongsToMany(Subject::class, 'student_subject', 'student_id', 'subject_id');
+        return $this->belongsTo(Subject::class, 'elective_subject_id');
     }
 
 
