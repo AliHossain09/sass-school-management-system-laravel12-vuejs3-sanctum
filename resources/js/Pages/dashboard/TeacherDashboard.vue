3f2158610300b30a2f7c1e578b4fd88d03cdcd66
AloHossain09 <alihoossainn@gmail.com>
2026-02-03 17:37:29 +0600

    setup Teacher dashboard

--- resources/js/Pages/dashboard/TeacherDashboard.vue
+++ resources/js/Pages/dashboard/TeacherDashboard.vue
@@ -1,14 +1,77 @@
-<template>
-  <SchoolLayout>
-    <h1 class="text-3xl font-bold mb-4">Teacher Dashboard</h1>
-    <p>Welcome, Teacher</p>
-  </SchoolLayout>
-</template>
-
-<script>
-import SchoolLayout from '../../layouts/SchoolLayout.vue'
-
-export default {
-  components: { SchoolLayout }
-}
+<script setup>
+import TeacherLayout from '../../layouts/TeacherLayout.vue'
 </script>
+
+<template>
+  <TeacherLayout>
+    <h1 class="text-2xl font-bold mb-6">Dashboard</h1>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+      <div class="bg-yellow-100 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Schools</p>
+        <p class="text-3xl font-bold">12</p>
+      </div>
+
+      <div class="bg-green-200 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Teachers</p>
+        <p class="text-3xl font-bold">240</p>
+      </div>
+
+      <div class="bg-gray-400 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Students</p>
+        <p class="text-3xl font-bold">240</p>
+      </div>
+    </div>
+<br>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div class="bg-green-100 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Teachers</p>
+        <p class="text-3xl font-bold">240</p>
+      </div>
+
+      <div class="bg-indigo-300 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Teachers</p>
+        <p class="text-3xl font-bold">240</p>
+      </div>
+
+      <div class="bg-orange-200 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Schools</p>
+        <p class="text-3xl font-bold">12</p>
+      </div>
+    </div>
+<br>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+      <div class="bg-yellow-100 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Schools</p>
+        <p class="text-3xl font-bold">12</p>
+      </div>
+
+      <div class="bg-green-200 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Teachers</p>
+        <p class="text-3xl font-bold">240</p>
+      </div>
+
+      <div class="bg-red-800 p-5 rounded shadow">
+        <p class="text-white">TeacherLayout Students</p>
+        <p class="text-3xl text-white font-bold">240</p>
+      </div>
+    </div>
+<br>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div class="bg-orange-200 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Schools</p>
+        <p class="text-3xl font-bold">12</p>
+      </div>
+
+      <div class="bg-indigo-300 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Teachers</p>
+        <p class="text-3xl font-bold">240</p>
+      </div>
+
+      <div class="bg-green-100 p-5 rounded shadow">
+        <p class="text-gray-500">TeacherLayout Teachers</p>
+        <p class="text-3xl font-bold">240</p>
+      </div>
+    </div>
+  </TeacherLayout> >
+</template>
---
+++ resources/js/layouts/TeacherLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <!-- NAVBAR -->
+    <header class="bg-blue-900 shadow flex items-center justify-between px-6 py-3">
+      <div class="flex items-center gap-3">
+        <button
+          class="xl:hidden text-2xl text-white"
+          @click="isSidebarOpen = true"
+        >
+          ☰
+        </button>
+
+        <h1 class="font-bold text-2xl text-white">
+          Teacher Dashboard
+        </h1>
+      </div>
+
+      <button
+        @click="logout"
+        class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+      >
+        Logout
+      </button>
+    </header>
+
+    <!-- BODY -->
+    <div class="flex flex-1 overflow-hidden">
+      <!-- Sidebar -->
+      <TeacherSidebar
+        :isOpen="isSidebarOpen"
+        @close="isSidebarOpen = false"
+      />
+
+      <!-- Main -->
+      <main class="flex-1 p-6 bg-gray-50 overflow-auto">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import TeacherSidebar from '../components/sidebar/TeacherSidebar.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const isSidebarOpen = ref(false)
+
+const logout = () => {
+  localStorage.clear()
+  router.push('/login')
+}
+</script>
